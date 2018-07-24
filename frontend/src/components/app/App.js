693e4ea5dbef16d1b84d558c58b88a38e7e0d923
Carl Olson <colson10@gmail.com>
2018-07-23 22:43:44 -0700

    updated app component to render differently based on if logged in

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    if (this.props.loggedIn) {
+      fetchUserLists();
+    }
+  }
+
   render() {
+    const loginView = <div><p className="App-intro">To get started, click the link below to login via Wunderlist.</p>
+    <h3><a href="https://www.wunderlist.com/oauth/authorize?client_id=450fc863964df672ed75&redirect_uri=http://localhost:7000/oauth/wunderlist&state=lksjdf;lkj092uf9j2flkj">Wunderlist Login</a></h3></div>;
+
+    const loggedInView = (
+    <ul>
+      { 
+        userLists.map((list, index) => {
+          return <li key={index} >{list.title} </li>;
+        })
+      }
+    </ul>
+    );
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Timeboxed  |  Wunderlist</h1>
         </header>
-        <p className="App-intro">
-          To get started, click the link below to login via Wunderlist.
-        </p>
-        <h3><a href="https://www.wunderlist.com/oauth/authorize?client_id=450fc863964df672ed75&redirect_uri=http://localhost:7000/oauth/wunderlist&state=lksjdf;lkj092uf9j2flkj">Wunderlist Login</a></h3>
+        { this.props.loggedIn ? loggedInView : loginView };
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  loggedIn: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+  loggedIn: !!state.token,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchUserLists: () => dispatch(userActions.fetchUserLists()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
